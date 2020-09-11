86705ea9946d6c0dea599ba0e7b6d9ac68e8c4e4
Aecio Pires <aecio.pires@sensedia.com>
2020-09-11 02:40:57 -0300

    Add exercise 3.5

--- src/aprendago/cap3/exercise3_5/exercise3_5.go
+++ src/aprendago/cap3/exercise3_5/exercise3_5.go
@@ -1,7 +1,5 @@
 package main
 
-// Reference: https://golang.org/ref/spec#Types
-
 import (
 	"fmt"
 )
@@ -9,9 +7,12 @@ import (
 type mytype int
 
 var x mytype
+var y int
 
 func main() {
 	fmt.Printf("Before x=%v (%T)\n", x, x)
 	x = 42
-	fmt.Printf("After x=%v (%T)", x, x)
+	fmt.Printf("After x=%v (%T)\n", x, x)
+	y = int(x)
+	fmt.Printf("y=%v (%T)", y, y)
 }
